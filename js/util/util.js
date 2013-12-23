5bff14edc18b06ea02c1743e484d630c1567ddca
davirussi <davirussi@gmail.com>
2013-12-23 11:26:12 -0200

    Loop jquery + pequenas mudanças de layout

--- js/util/util.js
+++ js/util/util.js
@@ -11,3 +11,16 @@
 		    document.getElementById("mainform2").style.display="";
 		}	
 	}
+	
+    var x=0;
+
+    function rotate(num){
+    fs=document.ff.slide;
+    x=num%fs.length;
+    if(x<0) x=fs.length-1;
+    document.images.show.src=fs.options[x].value;
+    fs.selectedIndex=x;}
+
+    function auto() {
+    if(document.ff.fa.value == "Stop"){
+    rotate(++x);setTimeout("auto()", 3000);}}
